a62c7e1ed2e72790ec986b60b3c49cd08af39319
Andry Ariadi <andryariadi23@gmail.com>
2025-01-22 13:29:41 +0700

    setup nextjs tailwind global css rootlayout

---
+++ src/constant/index.ts
@@ -0,0 +1,101 @@
+export const navLinks = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Promotions",
+    href: "/promotions",
+  },
+  {
+    title: "Blogs",
+    href: "/blogs",
+  },
+  {
+    title: "Contact Us",
+    href: "/contact",
+  },
+];
+
+export const categoryMenu = [
+  {
+    title: "Cupcake",
+    icon: "/cake.png",
+    items: 22,
+  },
+  {
+    title: "Pizza",
+    icon: "/pizza.png",
+    items: 22,
+  },
+  {
+    title: "Kebab",
+    icon: "/kebab.png",
+    items: 22,
+  },
+  {
+    title: "Salmon",
+    icon: "/salmon.png",
+    items: 22,
+  },
+  {
+    title: "Doughnut",
+    icon: "/donut.png",
+    items: 22,
+  },
+];
+
+export const trendingMenu = [
+  {
+    title: "Pizza Pepperoni",
+    img: "/paperoni-menu.png",
+    start: 4,
+    category: "Pizza",
+  },
+  {
+    title: "Pizza Meat",
+    img: "/pizza-menu.png",
+    start: 3,
+    category: "Pizza",
+  },
+  {
+    title: "Doner Kebab",
+    img: "/kebab-menu.png",
+    start: 5,
+    category: "Kebab",
+  },
+  {
+    title: "Salmon Roll",
+    img: "/salmon-menu.png",
+    start: 4,
+    category: "Salmon",
+  },
+  {
+    title: "Cupcake Choco",
+    img: "/cake-menu.png",
+    start: 4,
+    category: "Cupcake",
+  },
+  {
+    title: "Doughnut Milk",
+    img: "/donut-menu.png",
+    start: 5,
+    category: "Doughnut",
+  },
+  {
+    title: "Doughnut Unicorn",
+    img: "/pink-menu.png",
+    start: 4,
+    category: "Doughnut",
+  },
+  {
+    title: "Kathi Kebab",
+    img: "/kathi-menu.png",
+    start: 4,
+    category: "Kebab",
+  },
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: {
+          1: "#FFFFFF",
+          2: "rgba(255, 255, 255, 0.72)",
+          3: "rgba(255, 255, 255, 0.4)",
+          4: "rgba(255, 255, 255, 0.64)",
+          5: "rgba(255, 255, 255, 0.80)",
+        },
+        green: {
+          1: "#8BAC3E",
+          2: "#A4B441",
+        },
+        sky: "#40A2B1",
+        dark_blue: "#405EB6",
+        dark_pink: "#B23F74",
       },
     },
   },
